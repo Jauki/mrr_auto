2da90c7d99e6b2d7600a678d6d96224c102438a7
julianjauk <jaujuc18@htl-kaindorf.at>
2021-10-30 17:18:57 +0200

    init ⚙️

---
+++ main.py
@@ -0,0 +1,83 @@
+from googletrans import Translator
+from bs4 import BeautifulSoup
+import requests
+import os
+
+urlSen = "https://sentence.yourdictionary.com/"
+urlExp = "https://thesaurus.yourdictionary.com/"
+
+
+class MRRTask:
+    def __init__(self, src):
+        self.src = src
+
+    def translate(self):
+        translator = Translator(service_urls=['translate.googleapis.com'])
+        return translator.translate(self.src, dest="de").text
+        pass
+
+    def sentence(self):
+        try:
+            page = requests.get(urlSen + self.src)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            lists = soup.find_all('li', class_="sentences-list-item")
+            sentences = []
+            for item in lists:
+                box = item.find('div', class_="single-sentence-box").text
+                sentences.append(box)
+            return sentences[0]
+        except IndexError:
+            print(f'Some Words cant be converted! Look up yourself - Sentence Manager')
+            return '!!! X !!!'
+        pass
+
+    def synonym(self):
+        try:
+            page = requests.get(urlExp + self.src)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            lists = soup.find_all('div', class_="single-synonym-box")
+            synonyms = []
+            for item in lists:
+                box = item.find('div', class_="synonym").text
+                synonyms.append(box)
+            return synonyms[0]
+        except IndexError:
+            print(f'Some Words cant be converted! Look up yourself - Synonym Manager')
+            return '!!! X !!!'
+        pass
+
+    def out(self):
+        return f'| {self.src} | {self.sentence()} | {self.synonym()} | {self.translate()} |\n'
+
+
+def print_file(object_list):
+    try:
+        if os.path.isfile("out.md"):
+            os.remove("out.md")
+        f = open("out.md", "w")
+        f.write("| word | sentence | explanation | german |\n"
+                "|------|----------|-------------|--------|\n")
+        for obj in object_list:
+            f.write(obj.out())
+        f.close()
+        print("Done! Everything is stored in the 'out.md' File in your directory!")
+    except OSError:
+        print(f'Error!')
+
+
+def in_file():
+    file_name = "in.txt"
+    f = open(file_name)
+    input_text = f.read()
+    f.close()
+    return input_text.splitlines()
+
+
+# Main
+words = in_file()
+objects = []
+
+for word in words:
+    objects.append(MRRTask(word))
+
+print_file(objects)
---
+++ out.md
@@ -0,0 +1,12 @@
+| word | sentence | explanation | german |
+|------|----------|-------------|--------|
+| rebellion | Alva was now free to deal with rebellion in the north.   430     177       | resistance    (Biology) Ability (of an organism, tissue, or cell) to withstand a destructive agent or condition such as a chemical compound, a disease agent, or an environmental stressor:  | Rebellion |
+| mesmerising | !!! X !!! | !!! X !!! | hypnotisierend |
+| mockumentary | !!! X !!! | comedy-drama  (related)  | Mockumentary |
+| subconsciously | Many people subconsciously will back away from you if you get too close.   8     3       | unconsciously  (related)  | unbewusst |
+| gravel bike | !!! X !!! | !!! X !!! | Schotterrad |
+| pile | There really was a pile of bones in her tub.   235     76       | collection    A set of items or amount of material procured or gathered together.  | Haufen |
+| splashing | She sloshed through the slushy snow, splashing icy water on the legs of her jeans.   108     49       | splash    A small amount of liquid  | spritzen |
+| dieticians | Dieticians, cooks, wait staff, maintenance staff, front office personnel, accountants, financial aid advisors, marketing professionals, even wellness and activity/entertainment directors all help to keep the facilities efficient.   0     0       | !!! X !!! | Diätassistenten |
+| climax | That event was the climax of a long series of horrors.   384     105       | highest point    | Höhepunkt |
+| Sunday roast | !!! X !!! | !!! X !!! | Sonntagsbraten |
